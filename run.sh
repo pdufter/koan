34931539040deac140b03c6051c4f6ffc1d8401f
Philipp Dufter <philipp.dufter@gmail.com>
2021-01-27 18:47:49 +0100

    add eval script

--- run.sh
+++ run.sh
@@ -10,3 +10,17 @@
              --file /Users/philipp/Downloads/corpus.txt
 
 python ~/Dropbox/PhD/tools/projector/vis_embed_online.py /Users/Philipp/Dropbox/PhD/projects/koan/embeddings_2021-01-27_17:10:33.txt
+
+
+./build/koan -V 2000000 \
+             --epochs 10 \
+             --dim 20 \
+             --hidden 300 \
+             --negatives 5 \
+             --context-size 5 \
+             -l 0.075 \
+             --threads 30 \
+             --cbow true \
+             --min-count 2 \
+             --file ../pytorch-sgns/data/corpus.txt
+python ~/Dropbox/PhD/tools/projector/vis_embed_online.py /Users/philipp/Downloads/embeddings_2021-01-27_18:35:08.txt
